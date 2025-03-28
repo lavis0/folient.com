abb343abd7dc553e6e7ca59c7e6637054c4453b3
Joshua Banga <joshuabanga@hotmail.com>
2025-03-28 22:38:35 +0000

    Fix for staticfiles loading

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -166,8 +166,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # ]
 
 if DEBUG:
+  STATIC_URL = 'static/'
   STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
+  STATIC_URL = 'staticfiles/'
   STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
   STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
