e64731a3ff5641caafb98b14c9f421d04e650b53
Joshua Banga <joshuabanga@hotmail.com>
2025-03-28 19:12:29 +0000

    Fixed build files

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'blogApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
