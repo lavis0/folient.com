08b879da50d695f2e7ecd7b30dd4f79e2ca9adf1
Joshua Banga <joshuabanga@hotmail.com>
2025-03-28 17:08:47 +0000

    Editions to settings.py for production-readiness

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -177,8 +177,8 @@ else:
 # EMAIL CONFIGURATION (for contact form)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'youremail@gmail.com'
-EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
